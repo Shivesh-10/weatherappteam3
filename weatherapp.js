882b6c5d128501a6a3661bc3e6907173b6e2fc44
Shivesh-10 <107934490+Shivesh-10@users.noreply.github.com>
2022-06-21 19:22:04 +0530

    Update weatherapp.js

--- weatherapp.js
+++ weatherapp.js
@@ -1,4 +1,4 @@
-//Now we need to determine the constant of one of the id functions. Because no html function can be used directly in JavaScript.
+
 var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
 var city = document.querySelector('#cityoutput')
@@ -9,16 +9,16 @@ var wind = document.querySelector('#wind')
  
 apik = "3045dd712ffe6e702e3245525ac7fa38"
 
-//kelvin to celcious. 1 Kelvin is equal to -272.15 Celsius.
+
 
 function convertion(val){
     return (val - 273).toFixed(2)
 }
-//Now we have to collect all the information with the help of fetch method
+
 
     btn.addEventListener('click', function(){
 
-//This is the api link from where all the information will be collected
+
 
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
@@ -27,13 +27,13 @@ function convertion(val){
 
         .then(data => {
 
-//Now you need to collect the necessary information with the API link. Now I will collect that information and store it in different constants.
+
 
             var nameval = data['name']
             var descrip = data['weather']['0']['description']
             var tempature = data['main']['temp']
             var wndspd = data['wind']['speed']
-//Now with the help of innerHTML you have to make arrangements to display all the information in the webpage.
+
             city.innerHTML=`Weather of <span>${nameval}<span>`
             temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
             description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
@@ -41,7 +41,6 @@ function convertion(val){
 
         })
 
-//Now the condition must be added that what if you do not input anything in the input box.
+
         .catch(err => alert('You entered Wrong city name'))
     })
-//If you click on the submit button without inputting anything in the input box or typing the wrong city name then the above text can be seen.
